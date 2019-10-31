09999b2c5ca839b0520ec6980fda1a6fad96f4de
Dennis Effa <kamponsah.amponsah@gmail.com>
2019-10-31 08:28:20 +0000

    unit test for finding naughty student done

--- src/test/LectureTest.java
+++ src/test/LectureTest.java
@@ -1,6 +1,7 @@
 package test;
 
 import io.turntabl.java.Lecture;
+import io.turntabl.java.NaughtyStudent;
 import io.turntabl.java.Student;
 import org.junit.jupiter.api.Test;
 
@@ -16,10 +17,16 @@ class LectureTest {
       lt.enter(new Student(Arrays.asList(1.0,2.0)));
 
       assertEquals(5.0, lt.getHighestGrade());
+    }
 
+    @Test
+    public void testNaughtyStudent(){
+        Lecture lecture = new Lecture();
+        lecture.enter(new Student(Arrays.asList(2.0,4.0)));
+        lecture.enter(new Student(Arrays.asList(4.0,2.0)));
+        lecture.enter( new NaughtyStudent(Arrays.asList(2.0,4.0)));
 
-
-
+        assertNotEquals(3, lecture.getHighestGrade());
     }
 
 }
