8e483531614f4cd5b4e4d28042406f7348e00f5a
Dennis Effa <kamponsah.amponsah@gmail.com>
2019-10-31 09:48:45 +0000

    implemented streams

--- src/io/turntabl/java/Lecture.java
+++ src/io/turntabl/java/Lecture.java
@@ -5,19 +5,21 @@ import java.util.List;
 
 public class Lecture {
      List<Student> students = new ArrayList<>();
-
+     
     public void enter(Student std) {
         this.students.add(std);
     }
 
     public Double getHighestGrade() {
         Double highest =0.0;
-        for (Student std: students
+        /*for (Student std: students
              ) {
             if (std.getAverageGrade() > highest) {
                 highest = std.getAverageGrade();
             }
         }
+         */
+        highest = students.stream().mapToDouble(student -> student.getAverageGrade()).max().orElseThrow();
         return highest;
     }
 }
